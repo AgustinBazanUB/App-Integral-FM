fc313ec72d863820942c734e07c86ea0fe190e0e
AgustinBazanUB <167490338+AgustinBazanUB@users.noreply.github.com>
2026-08-03 17:45:03 -0300

    Separa Firebase para App Integral FM

--- src/gestion/services/firebase.js
+++ src/gestion/services/firebase.js
@@ -11,20 +11,20 @@ import { getFirestore } from "firebase/firestore";
 export const firebaseConfig = {
   apiKey:
     import.meta.env.VITE_FIREBASE_API_KEY ||
-    "AIzaSyD-GEqaTJZKXyKa7fUBbXa0Cigit7qZyio",
+    "AIzaSyCBVuIt36d_0DbDdEkGwRy85hmpZEjbrVg",
   authDomain:
     import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ||
-    "fm-stock-y-venta.firebaseapp.com",
+    "app-integral-fm.firebaseapp.com",
   projectId:
-    import.meta.env.VITE_FIREBASE_PROJECT_ID || "fm-stock-y-venta",
+    import.meta.env.VITE_FIREBASE_PROJECT_ID || "app-integral-fm",
   storageBucket:
     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ||
-    "fm-stock-y-venta.firebasestorage.app",
+    "app-integral-fm.firebasestorage.app",
   messagingSenderId:
-    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "334694189931",
+    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "447903189609",
   appId:
     import.meta.env.VITE_FIREBASE_APP_ID ||
-    "1:334694189931:web:6f75a7f73e2eacd3675a2e",
+    "1:447903189609:web:ffab0ee93aa45f7fe5b0f8",
 };
 
 export const firebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
@@ -39,3 +39,4 @@ export const login = (email, password) =>
   signInWithEmailAndPassword(auth, email.trim(), password);
 
 export const logout = () => signOut(auth);
+
